fc0bf91ae43d731343870cc16ba3e9d52c9ffbf1
Noah Parker <noahtmparker@gmail.com>
2022-08-20 11:00:29 -0400

    created new note and delete functions / modularized files

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require("./routes/apiRoutes/index");
+const htmlRoutes = require("./routes/htmlRoutes/index");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
