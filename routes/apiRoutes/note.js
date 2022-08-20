72b28e87dd99fac1bc221811598132a411fbeaef
Noah Parker <noahtmparker@gmail.com>
2022-08-20 11:37:09 -0400

    created GET, POST and DELETE routes

--- routes/apiRoutes/note.js
+++ routes/apiRoutes/note.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const { notes } = require("../../Develop/db/db.json");
+const { createNote, deleteNote } = require("../../lib/notes");
+
+router.get("/note", (req, res) => {
+  let saved = notes;
+  res.json(saved);
+});
+
+router.post("./note", (req, res) => {
+  req.body.id = notes.length.toString();
+  let note = createNote(req.body, notes);
+  res.json(notes);
+});
+
+router.delete("/note/:id", (req, res) => {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
+});
+
+module.exports = router;
